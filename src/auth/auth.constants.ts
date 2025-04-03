1113592f7a158d74f7bd429a3cc5159d6b1c0665
Yossi Shasho <jozard00@gmail.com>
2025-04-03 13:51:49 +0300

    support a sync llanaAuthCheck in the client, cookie based

--- src/auth/auth.constants.ts
+++ src/auth/auth.constants.ts
@@ -2,3 +2,4 @@ export const JWT = 'jwt'
 
 export const ACCESS_TOKEN_COOKIE_NAME = 'accessToken'
 export const REFRESH_TOKEN_COOKIE_NAME = 'refreshToken'
+export const IS_LOGGED_IN_COOKIE_NAME = 'isLlanaLoggedIn'
